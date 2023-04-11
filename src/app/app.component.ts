6f9f0f71c50b95a915fe876435fa209da9682ea6
Maxime Laroche <mlaroche2009@gmail.com>
2023-04-11 10:33:03 -0400

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+updateLenght($event: Event) {
+  console.log($event)
+}
+  title = 'password-generator';
+  constructor(private clipboard: Clipboard){}
+
+  options: { name: string, activated: boolean, rule: string }[] = [
+    { name: 'Nombres', activated: true, rule: '0123456789' },
+    { name: 'Lettres', activated: true, rule: 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm' },
+    { name: 'Accents', activated: false, rule: 'êûîôâȩşçëüïöäèùìòàé' },
+    { name: 'Charactètres spécianx standarts', activated: true, rule: '!@[]{}/$%?&*()-=_+' },
+    { name: 'Characte`res spéciaux vraiment standarts', activated: false, rule: '|±@£¢¤¬¦²³¼½¾~µé' }];
+  length: number = 32;
+  generated: boolean = false;
+  
+  generatePassword() {
+    let possible_letters = '';
+    for (let option of this.options) {
+      if (option.activated) {
+        possible_letters += option.rule;
+      }
+    }
+    let password = ''
+    for (let i = 0; i < this.length; i++){
+      password += possible_letters.charAt(Math.floor(Math.random() * possible_letters.length));
+    } 
+    this.clipboard.copy(password);
+    this.generated = true;
+    setTimeout(() => {
+      this.generated = false;
+    }, 3000);
+  }
+
+  
+}
